966651f7b08ec58e34f5cb8dd5353c144cb5895c
Greentaylor27 <greentaylor18@gmail.com>
2025-07-23 15:56:10 -0500

    Okay this is the new site for Capstone.
    Using vite to create the base files.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center'>
+      Hello we have tailwind now
+
+      WE HAVE TAILWIND!!!!!
+    </div>
+  )
+}
+
+export default App
