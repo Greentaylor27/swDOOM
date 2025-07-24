8b0010ae757cbde128f476c542d89558e1a34aab
lrwyatt801 <loganwyatt1995@gmail.com>
2025-07-24 18:41:56 -0500

    add canvas, not tested

--- Components/Screen.tsx
+++ Components/Screen.tsx
@@ -2,7 +2,14 @@ export default function Screen() {
   {/* the canvas will go here */}
   return (
     <div>
-      Hello (The canvas will go here!)
+	<canvas id="canvas" oncontextmenu="event.preventDefault()"></canvas>
+	<script type='text/javascript'>
+		 var Module = {
+		 	canvas: (function() {return document.getElementById('canvas'); })()
+		 };
+	</script>
+
+	<script src="index.js"></script> <!-- TODO: fix this line -->
     </div>
   )
 }
