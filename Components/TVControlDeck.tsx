0d6d1ca0f60ea05149a34b3113b6ec9dd3678b32
Greentaylor27 <greentaylor18@gmail.com>
2025-07-25 19:37:37 -0500

    Created a web dev branch and laid out. Struggled getting my OS to work but it is and I have something to add here :)

---
+++ Components/TVControlDeck.tsx
@@ -0,0 +1,24 @@
+export default function TVControlDeck(){
+    <div>
+        {/* here is where I will put the buttons vcr deck and everything else below the screen */}
+        <div className="plugs">
+            {/* Here is where I will put the video and audio plugs */}
+            {/* <VideoPlug /> */}
+            {/* <AudioPlugs /> */}
+        </div>
+        <div className="buttonGroups">
+            {/* Here is where I will put the TV buttons */}
+            {/* <PowerButton /> */}
+            {/* <VolumeControl /> */}
+            {/* <ChannelControl /> */}
+        </div>
+
+
+        {/* Stretch goal idea VCR deck
+        <div className="VCRDeck">
+            <VCRDeck />
+        </div>
+        */}
+
+    </div>
+}
