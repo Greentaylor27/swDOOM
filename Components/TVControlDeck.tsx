c52b3d44d7e6a9ff4251d9369e3b996e71a13f48
Greentaylor27 <greentaylor18@gmail.com>
2025-07-26 16:42:37 -0500

    Updating comments.
    Updating comments to be more clear and what the intentions are and what is and isn't MVP or stretch goal

--- Components/TVControlDeck.tsx
+++ Components/TVControlDeck.tsx
@@ -6,13 +6,16 @@ export default function TVControlDeck() {
   return (
     <div className="TVControlDeck">
       <div className="plugs">
-        {/* Here I will put the video and audio plugs */}
+        {/* Here I will put the video and audio plugs, these are only visual components */}
         <VideoPlug />
         <LeftAudioPlug />
         <RightAudioPlug />
       </div>
       <div className="buttonGroup">
-        {/* Here is where I will put the TV Buttons */}
+        {/* Here is where I will put the TV Buttons, 
+        (stretch goal idea allow channel control to switch channels which corresponds to a shader.)
+        */}
+
         {/* <PowerButton /> */}
         {/* <VolumeControl /> */}
         {/* <ChannelControl /> */}
