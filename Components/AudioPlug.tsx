070f8a05ec174bf5c902307198032ddb0ebb7da7
Greentaylor27 <greentaylor18@gmail.com>
2025-07-26 09:12:28 -0500

    All the input plugs have been made.
    Time to style and place them properly.

--- Components/AudioPlug.tsx
+++ Components/AudioPlug.tsx
@@ -2,8 +2,14 @@ export default function AudioPlugs() {
     return (
     <div className="audioPlugs">
     {/* This is a Visual component for the audio plugs. Can add functionality later */}
-      <div className="leftPlug"></div>
-      <div className="rightPlug"></div>
+      <div className="plugOuterShell">
+        <div className="audioPlugLeft"></div>
+        <div className="plugHole"></div>
+      </div>
+      <div className="plugOuterShell">
+        <div className="audioPlugRight"></div>
+        <div className="plugHole"></div>
+      </div>
     </div>
   )
 }
--- Components/VideoPlug.tsx
+++ Components/VideoPlug.tsx
@@ -1,6 +1,6 @@
 export default function VideoPlug() {
   return (
-    <div className="plugOuterShell">
+    <div className="plugOuterShell" id="videoPlug">
       {/* This is a Visual component for the video plugs. Can add functionality later */}
       <div className="plugVideo" />
       <div className="plugHole" />
