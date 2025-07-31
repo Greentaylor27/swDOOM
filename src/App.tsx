0d1c4de2bd01c0ddab4c3c9fb24902330bf342e4
Greentaylor27 <greentaylor18@gmail.com>
2025-07-31 14:42:28 -0500

    Updates to website. Finished up Television look need to add speaker grille and the canvas to the page. Started development on the mobile site

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 import Television from '../Components/Television';
+import GamePad from '../Components/GamePad';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isMonitor, setIsMonitor] = useState(window.innerWidth > 1024);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMonitor(window.innerWidth > 1024);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
+  
   return (
     <div className='app'>
-      <Television />
+      {isMonitor ? <Television /> : <GamePad />}
     </div>
   )
 }
